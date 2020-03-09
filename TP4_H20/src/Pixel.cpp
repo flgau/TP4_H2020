7a82316946756c4e36589e27df2a481ffed89980
Felix Gauthier <filgauthier03@gmail.com>
2020-03-09 19:37:24 -0400

    ajout des set rouge, bleu et vert

--- TP4_H20/src/Pixel.cpp
+++ TP4_H20/src/Pixel.cpp
@@ -1,5 +1,5 @@
 #include "Pixel.h"
-
+#include <cmath>
 /**
  * @brief constructeur par défaut de la classe
  */
@@ -15,25 +15,56 @@ Pixel::Pixel(uint8_t rouge, uint8_t vert, uint8_t bleu)
 
 void Pixel::operator=(const Pixel &pixel) {
     rouge_ = pixel.rouge_;
-    vert
+    vert_ = pixel.vert_;
+    bleu_ = pixel.bleu_;
 }
 
 void Pixel::setRouge(int rouge) {
   // TO DO
+  //pas certain que cest ca qui faut faire mais cest ce que jai compris
+    //int five_divided_by_x = (x != 0 ? 5 / x : 0);
+    int test_si_rouge_trop_grand = (rouge > 255 ? rouge = 255 : rouge = rouge);
+    int test_si_rouge_trop_petit = (rouge < 0 ? rouge = 0 : rouge = rouge);
+    rouge_ = rouge;
+    /* mettons rouge = 300
+    apres la premiere "fonction" rouge = 255
+    apres la deuxieme 'fonction" rouge qui valait 255 vaut encore 255
+    finalement rouge_ = rouge // rouge =255
+    */
+
+    /*
+    mettons rouge = -10
+    apres la premiere fonction rouge vaut -10
+    apres la deuxieme fonction rouge vaut 0
+    finalement rouge_=rouge //rouge = 0
+
+    */
+
+
+    
 }
 
 void Pixel::setVert(int vert) {
   // TO DO
+    int test_si_vert_trop_grand = (vert > 255 ? vert = 255 : vert = vert);
+    int test_si_vert_trop_petit = (vert < 0 ? vert = 0 : vert = vert);
+    vert_ = vert;
+
+
 }
 
 void Pixel::setBleu(int bleu) {
-  // TO DO
+  int test_si_bleu_trop_grand = (bleu > 255 ? bleu = 255 : bleu = bleu);
+  int test_si_bleu_trop_petit = (bleu < 0 ? bleu = 0 : bleu = bleu);
+  bleu_ = bleu;
+
 }
 /**
  * @brief retourn l'attribut rouge_ du pixel
  * @return rouge_ du type uint8_t
  */
 uint8_t Pixel::getRouge() const { return rouge_; }
+
 /**
  * @brief retourn l'attribut vert_ du pixel
  * @return vert_ du type uint8_t
